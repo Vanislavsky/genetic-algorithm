6681dd2a519cba3431aa4b0dff23b0197ce530f4
Vanislavsky <serzh7214@mail,ru>
2023-12-22 00:00:35 +1000

    refactoring code

---
+++ genetic_algorithm.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+
+class Generic_algorithm:
+    def __init__(self,
+                 *,
+                 tasks_levels: np.ndarray,
+                 count_tasks: int,
+                 count_engineer: int,
+                 population_size: int,
+                 time_array: np.ndarray,
+                 coeff_array: np.ndarray,
+                 selection_size: int,
+                 p_mutation_ind: float,
+                 p_mutation_gen: float,
+                 k_point: int = 1,
+                 fitness_by_total_time: bool = False):
+        self.tasks_levels = tasks_levels
+        self.count_tasks = count_tasks
+        self.count_engineer = count_engineer
+        self.population_size = population_size
+        self.time_array = time_array
+        self.coeff_array = coeff_array
+        self.selection_size = selection_size
+        self.p_mutation_ind = p_mutation_ind
+        self.p_mutation_gen = p_mutation_gen
+        self.k_point = k_point
+        self.fitness_by_total_time = fitness_by_total_time
+
+        self._rng = np.random.default_rng()
+        self._population = self._rng.integers(low=0, high=self.count_engineer, size=(self.population_size, self.count_tasks))
+
+    def fitness_max_time_by_engineer(self):
+        ret_score = []
+        for individual in self._population:
+            enginer_tasks_sums = {}
+            for i, enj in enumerate(individual):
+                if enj in enginer_tasks_sums:
+                    enginer_tasks_sums[enj] += self.time_array[i] * self.coeff_array[enj][self.tasks_levels[i] - 1]
+                else:
+                    enginer_tasks_sums[enj] = self.time_array[i] * self.coeff_array[enj][self.tasks_levels[i] - 1]
+            ret_score.append(enginer_tasks_sums[max(enginer_tasks_sums, key=enginer_tasks_sums.get)])
+
+        return ret_score
+
+    def fintess_total_time(self):
+        ret_score = []
+        for individual in self._population:
+            temp_score = []
+            k = 0
+            enginer_tasks_sums = {}
+            for i, enj in enumerate(individual):
+                if k <= self.count_engineer:
+                    if enj in enginer_tasks_sums:
+                        enginer_tasks_sums[enj] += self.time_array[i] * self.coeff_array[enj][self.tasks_levels[i] - 1]
+                    else:
+                        enginer_tasks_sums[enj] = self.time_array[i] * self.coeff_array[enj][self.tasks_levels[i] - 1]
+                    k += 1
+                if k == self.count_engineer or i == len(individual) - 1:
+                    temp_score.append(enginer_tasks_sums[max(enginer_tasks_sums, key=enginer_tasks_sums.get)])
+                    k = 0
+                    enginer_tasks_sums.clear()
+            ret_score.append(sum(temp_score))
+
+        return ret_score
+
+    def fitness(self):
+        if self.fitness_by_total_time:
+            return self.fintess_total_time()
+        else:
+            return self.fitness_max_time_by_engineer()
+
+    def selection(self):
+        self._selected_population = self._population[np.argsort(self.fitness())][:self.selection_size]
+        return self._selected_population
+
+    def crossover(self):
+        n_children = self.population_size - self.selection_size
+        idx1 = self._rng.integers(low=0, high=self.selection_size, size=n_children)
+        shift = self._rng.integers(low=1, high=self.selection_size - 1, size=n_children)
+        idx2 = (idx1 + shift) % self.selection_size
+
+
+        slice_points = self._rng.integers(low=1, high=self.count_tasks - 1, size=(self.k_point, n_children))
+        slice_points.sort(axis=0)
+        mask = [False for i in range(self.count_tasks)]
+        all_points = np.arange(self.count_tasks)[np.newaxis]
+
+        for i, slice_point in enumerate(slice_points):
+            if i % 2 == 1:
+                continue
+            if i == 0:
+                mask = np.logical_or(mask, np.logical_and(all_points >= 0, all_points < slice_point[:, np.newaxis]))
+            elif i == slice_points.shape[0] - 1:
+                mask = np.logical_or(mask, np.logical_and(all_points > slice_point[:, np.newaxis], all_points < self.count_tasks))
+            else:
+                mask = np.logical_or(mask, np.logical_and(all_points > slice_points[i][:, np.newaxis],  all_points < slice_points[i+1][:, np.newaxis]))
+
+        self._new_generation = np.where(
+            mask, # (1, max_len_individual) < (n_children, 1) => (n_children, max_len_individual)
+            self._selected_population[idx1],
+            self._selected_population[idx2]
+        )
+
+        return self._new_generation
+
+    def mutation(self):
+        mask = self._rng.random(self._new_generation.shape[0]) <= self.p_mutation_ind
+        mutation_children = self._new_generation[mask]
+        self._new_generation[mask] = np.where(
+            self._rng.random(size=mutation_children.shape) <= self.p_mutation_gen,
+            self._rng.integers(low=0, high=self.count_engineer, size=mutation_children.shape),
+            mutation_children
+        )
+        return self._new_generation
+
+    def step(self):
+        self.selection()
+        self.crossover()
+        self.mutation()
+        self._population = np.concatenate([self._selected_population, self._new_generation], axis=0)
+
+    def best_individuals(self, count):
+        return self._population[:count] + 1
--- main.py
+++ main.py
@@ -1,150 +1,12 @@
 import numpy as np
+import genetic_algorithm
 
-class Generic_algorithm:
-    def __init__(self,
-                 tasks,
-                 count_tasks,
-                 count_engineer,
-                 population_size,
-                 time_array,
-                 coeff_array,
-                 size_selection,
-                 p_mutation_ind: float,
-                 p_mutation_gen: float,
-                 k_point=1):
-        self.tasks = tasks
-        self.count_tasks = count_tasks
-        self.count_engineer = count_engineer
-        self.rng = np.random.default_rng()
-        self.population_size = population_size
-        self.time_array = time_array
-        self.coeff_array = coeff_array
-        self.size_selection = size_selection
-        self.p_mutation_ind = p_mutation_ind
-        self.p_mutation_gen = p_mutation_gen
-        self.k_point = k_point
-
-        self.population = self.rng.integers(low=0, high=self.count_engineer, size=(self.population_size, self.count_tasks))
-
-    def fitness(self):
-        ret_score = []
-        for individual in self.population:
-            temp_score = []
-            k = 0
-            helper_sum = {}
-            temp_sum = 0
-            # for i, enj in enumerate(individual):
-            #     if k <= self.count_engineer:
-            #         if enj in helper_sum:
-            #             helper_sum[enj] += self.time_array[i] * self.coeff_array[enj][self.tasks[i] - 1]
-            #         else:
-            #             helper_sum[enj] = self.time_array[i] * self.coeff_array[enj][self.tasks[i] - 1]
-            #         k += 1
-            #     if k == self.count_engineer or i == len(individual) - 1:
-            #         temp_score.append(helper_sum[max(helper_sum, key=helper_sum.get)])
-            #         k = 0
-            #         helper_sum.clear()
-            #         temp_sum = 0
-            # ret_score.append(sum(temp_score))
-            for i, enj in enumerate(individual):
-                if enj in helper_sum:
-                    helper_sum[enj] += self.time_array[i] * self.coeff_array[enj][self.tasks[i] - 1]
-                else:
-                    helper_sum[enj] = self.time_array[i] * self.coeff_array[enj][self.tasks[i] - 1]
-            ret_score.append(helper_sum[max(helper_sum, key=helper_sum.get)])
-
-
-        return ret_score
-
-    def selection(self):
-        self.selected_population = self.population[np.argsort(self.fitness())][:self.size_selection]
-        return self.selected_population
-
-    def crossover(self):
-        n_children = self.population_size - self.size_selection
-        idx1 = self.rng.integers(low=0, high=self.size_selection, size=n_children)
-        shift = self.rng.integers(low=1, high=self.size_selection - 1, size=n_children)
-        idx2 = (idx1 + shift) % self.size_selection
-        all_points = np.arange(self.count_tasks)[np.newaxis]
-
-
-        slice_points = self.rng.integers(low=1, high=self.count_tasks - 1, size=(self.k_point, n_children))
-        slice_points.sort(axis=0)
-        mask = [False for i in range(self.count_tasks)]
-        for i, slice_point in enumerate(slice_points):
-            if i == 0:
-                mask = np.logical_or(mask, np.logical_and(all_points >= 0, all_points < slice_point[:, np.newaxis]))
-            elif i == slice_points.shape[0] - 1:
-                if i % 2 == 1:
-                    continue
-                mask = np.logical_or(mask, np.logical_and(all_points > slice_point[:, np.newaxis], all_points < self.count_tasks))
-            else:
-                if i % 2 == 1:
-                    continue
-                mask = np.logical_or(mask, np.logical_and(all_points > slice_points[i][:, np.newaxis],  all_points < slice_points[i+1][:, np.newaxis]))
-
-
-
-        slice_point = self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)
-        slice_point1 = self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)
-        slice_point2 = self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)
-        for i in range(slice_point1.size):
-            slice_point1[i] = min(slice_point1[i], slice_point2[i])
-            slice_point2[i] = max(slice_point1[i], slice_point2[i])
-        a = np.logical_and(all_points > slice_point1[:, np.newaxis], all_points < slice_point2[:, np.newaxis])
-        self.new_generation = np.where(
-            mask, # (1, max_len_individual) < (n_children, 1) => (n_children, max_len_individual)
-            self.selected_population[idx1],
-            self.selected_population[idx2]
-        )
-        # slice_points = []
-        # for i in range(self.k_point):
-        #     a = self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)
-        #     slice_points.append(np.array(self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)))
-        # slice_points = np.array(slice_points)
-        # slice_points.sort(axis=-1)
-        # result_mask = []
-        # slice_point1 = self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)
-        # for i, slice_point in enumerate(slice_points):
-        #     if i == 0:
-        #         result_mask = np.arange(self.count_tasks)[np.newaxis] <= slice_point[:, np.newaxis]
-        #     else:
-        #         if i % 2 == 1:
-        #             result_mask = np.logical_or(result_mask, np.arange(self.count_tasks)[np.newaxis] > slice_point[:, np.newaxis])
-        #         else:
-        #             result_mask = np.logical_or(result_mask, np.arange(self.count_tasks)[np.newaxis] <= slice_point[:, np.newaxis])
-        # slice_point1 = self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)
-        # slice_point2 = self.rng.integers(low=1, high=self.count_tasks - 1, size=n_children)
-        # for i in range(slice_point1.size):
-        #     slice_point1[i] = min(slice_point1[i], slice_point2[i])
-        #     slice_point2[i] = max(slice_point1[i], slice_point2[i])
-        #
-        # self.new_generation = np.where(
-        #     result_mask,
-        #     self.selected_population[idx1],
-        #     self.selected_population[idx2]
-        # )
-
-        return self.new_generation
-
-    def mutation(self):
-        mask = self.rng.random(self.new_generation.shape[0]) <= self.p_mutation_ind
-        mutation_children = self.new_generation[mask]
-        self.new_generation[mask] = np.where(
-            self.rng.random(size=mutation_children.shape) <= self.p_mutation_gen,
-            self.rng.integers(low=0, high=self.count_engineer, size=mutation_children.shape),
-            mutation_children
-        )
-        return self.new_generation
-
-    def step(self):
-        self.selection()
-        self.crossover()
-        self.mutation()
-        self.population = np.concatenate([self.selected_population, self.new_generation], axis=0)
-
-
-
+NUM_EPOCH = 1000
+POPULATION_SIZE = 200
+SELECTION_SIZE = 60
+P_MUTATION_IND = 0.3
+P_MUTATION_GEN = 0.2
+K_POINT = 100
 
 
 peoples_coeff = []
@@ -155,10 +17,23 @@ times = np.array(list(map(float, fin.readline().split())))
 m = int(fin.readline())
 for i in range(m):
     peoples_coeff.append(list(map(float, fin.readline().split())))
-#print(peoples_coeff)
+peoples_coeff = np.array(peoples_coeff)
 
-ga = Generic_algorithm(tasks, n, m, 200, times, peoples_coeff, 60, 0.3, 0.2, 100)
-for i in range(1000):
-    print(i)
+ga = genetic_algorithm.Generic_algorithm(
+    tasks_levels=tasks,
+    count_tasks=n,
+    count_engineer=m,
+    population_size=POPULATION_SIZE,
+    time_array=times,
+    coeff_array=peoples_coeff,
+    selection_size=SELECTION_SIZE,
+    p_mutation_ind=P_MUTATION_IND,
+    p_mutation_gen=P_MUTATION_GEN,
+    k_point=K_POINT)
+
+
+for i in range(NUM_EPOCH):
+    print("Epoch: ", i)
     ga.step()
-print(ga.population[0] + 1)
+
+print(ga.best_individuals(1))
